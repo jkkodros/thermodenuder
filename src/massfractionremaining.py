0a17f375abe0286b36da527c7e95863f53dc03a8
J Kodros <jkkodros.research@gmail.com>
2020-10-20 15:50:08 +0300

    First attempt at basic CRT model

--- src/massfractionremaining.py
+++ src/massfractionremaining.py
@@ -7,11 +7,11 @@ TD simulation
 
 @author: J Kodros
 """
-
+import numpy as np
 
 class MassFractionRemaining:
     def __init__(self, T_TD, MFR, c_aer=None, dp=None, density=1300,
-                 MW=0.17, sigma=0.05):
+                 MW=0.17, sigma=0.05, alpha=1.0):
         '''
         Units are assumed to be SI.
         T in [K], c_aer in [kg m-3], dp in [m], density in [kg m-3]
@@ -21,6 +21,12 @@ class MassFractionRemaining:
         self.MFR = MFR
         self.c_aer = None
         self.dp = dp
+        self.rp = dp/2.0
         self.density = density
         self.MW = MW
         self.sigma = sigma
+        self.alpha = 1.0
+
+    def mass_per_particle(self):
+        mass_per_particle = self.density * 4.0 * np.pi * self.rp**3 / 3.0
+        return mass_per_particle
--- src/simulate_thermodenuder.py
+++ src/simulate_thermodenuder.py
@@ -7,11 +7,166 @@ Simulate TD for single OA compounds
 @author: J Kodros
 """
 
-from pylab import *
+import numpy as np
 from scipy import integrate
+
 from src import fluxes as fx
+from src import constants as ctd
 
 
-def fitTD(T_TD, cstar, dHvap, alpha, dp_i, c_aer_tot_i, rho\
-          , l_heat, D_heat, D_cool, l_cool, flow_ams, flow_smps\
-          , MW=0.2, sigma=0.05, T_ref=298.15, T_i=298.15, T_cool=297.15):
+def calc_number_from_mass(mfr):
+    '''
+    Calculate total number from total OA mass assuming density and
+    spherical particles.
+    '''
+    n_tot = mfr.c_aer / (4.0 * mfr.density * mfr.rp**3 * np.pi/3.)
+    return n_tot
+
+
+def pstar_from_cstar(cstar, mfr, td, constants):
+    '''
+    Calculate saturation vapor concentration based off the given
+    saturation mass concentration (cstar)
+    '''
+    pstar = (cstar * constants.IDEAL_GAS * td.T_ref) / mfr.MW
+    return pstar
+
+
+def calc_saturation_pressure_at_temperature(pstar, dh, td, constants):
+    '''
+    Calculate saturation vapor pressure with temperature dependence
+    '''
+    psat_i = pstar * np.exp(dh * ((1.0/td.T_ref) - (1.0/td.T_i)) /
+                            constants.IDEAL_GAS)
+    return psat_i
+
+
+def kelvin_effect_at_initial_temperature(mfr, td, constants):
+    '''
+    Calculate Kelvin effect at the initial TD temperature
+    '''
+    Ke_i = np.exp(2.0 * mfr.MW * mfr.sigma / (
+        constants.IDEAL_GAS * td.T_i * mfr.density * mfr.rp))
+    return Ke_i
+
+
+def equilibrium_pressure(psat, Ke):
+    '''
+    Calculate equilibrium pressure
+    '''
+    peq = psat * Ke
+    return peq
+
+
+def vapor_mass_concentration(peq, mfr, constants):
+    '''
+    Calculate the vapor concentration at the initial temperature
+    '''
+    cgas = peq * mfr.MW / (constants.IDEA_GAS * mfr.T_TD[0])
+    return cgas
+
+
+def get_time_array(td, heating=True, n=1000, t0=0):
+    '''
+    Get time and dt arrays for the integration
+    '''
+    if heating:
+        crt = td.calc_heating_section_crt()
+    else:
+        crt = td.calc_cooling_section_crt()
+    time = np.linspace(t0, crt, n)
+    dt = np.mean(time[1:] - time[:-1])
+    return time, dt
+
+
+def initiate_integrator():
+    '''
+    Setup the scipy integrator
+    '''
+    r = integrate.ode(fx.calc_fluxes).set_integrator("dopri5")
+    return r
+
+
+def set_integrator_params(r, dt, length, crt, T, n_tot_i, pstar, dh, time, y0,
+                          td, mfr, constants):
+    '''
+    Set the integrator parameters
+    '''
+    r.set_f_params(dt, length, crt, T, td.T_i, n_tot_i, pstar, dh, td.T_ref,
+                   mfr.MW, mfr.sigma, mfr.density, constants.DIFFUSION_COEF,
+                   constants.MU, constants.PRESSURE, mfr.alpha,
+                   constants.IDEAL_GAS)
+    r.set_initial_value(y0, time[0])
+    return r
+
+
+def simulate_one_temperature(mp_in, gc_in, T, r, dt, time, length, crt,
+                             n_tot_i, pstar, dh, td, mfr, constants):
+    '''
+    Simulate the TD at a given temperature.
+    Returns the estimate aerosol mass concentration at the end of the section
+    Note: This only does one section (not both heating and cooling)
+    '''
+    y0 = np.array([mp_in, gc_in])
+    y = np.zeros((len(time), len(y0)))
+    y[0, :] = y0
+
+    r = set_integrator_params(r, dt, length, crt, T, n_tot_i, pstar, dh,
+                              time, y0, td, mfr, constants)
+
+    for i in range(1, time.size):
+        y[i, :] = r.integrate(time[i])
+        if not r.successful():
+            raise RuntimeError("Could not integrate")
+
+    Pc_t_k = y[:, 0]
+    Gc_t_k = y[:, 1]
+
+    # Check for bad values
+    Pc_t_k = np.ma.masked_less(Pc_t_k, 0.0).filled(0.0)
+    Gc_t_k = np.ma.masked_less(Gc_t_k, 0.0).filled(0.0)
+
+    c_aer_out = n_tot_i * Pc_t_k[-1]
+    return c_aer_out
+
+
+def fitTD(mfr, td, cstar, dh):
+    # Initialize object containing useful thermodynamic constants
+    constants = ctd.ThermodynamicConstants()
+    # Initial number concentration of OA
+    n_tot_i = calc_number_from_mass(mfr)
+    # Saturation vapor pressure (from given cstar)
+    pstar = pstar_from_cstar(cstar, mfr, td, constants)
+    # Saturation vapor pressure at initial temperature
+    psat_i = calc_saturation_pressure_at_temperature(pstar, dh, td, constants)
+    # Kevlin effect at initial temperature and radius
+    Ke_i = kelvin_effect_at_initial_temperature(mfr, td, constants)
+    # Equilibrium pressure above particle surface
+    peq_i = equilibrium_pressure(psat_i, Ke_i)
+    # Mass per particle
+    mp_i = mfr.mass_per_particle()
+    # Vapor mass concentration
+    cgas_i = vapor_mass_concentration(peq_i, mfr, constants)
+    # Get time arrays
+    time_heat, dt_heat = get_time_array(td, heating=True)
+    time_cool, dt_cool = get_time_array(td, heating=False)
+    # Output MFR array
+    MFR_SIM = np.zeros(len(mfr.T_TD))
+    # Initiate integrator
+    r = initiate_integrator()
+
+    # Loop through temperatures for heating section
+    crt_heat = td.calc_heating_section_crt()
+    crt_cool = td.calc_cooling_section_crt()
+    for k, T in enumerate(mfr.T_TD):
+        c_aer_out_heat = simulate_one_temperature(
+            mp_i, cgas_i, T, r, dt_heat, time_heat, td.l_heat, crt_heat,
+            n_tot_i, pstar, dh, td, mfr, constants)
+
+        c_aer_out = simulate_one_temperature(
+            c_aer_out_heat, 0.0, td.T_cool, r, dt_cool, time_cool, td.l_cool,
+            crt_cool, n_tot_i, pstar, dh, td, mfr, constants)
+
+        MFR_SIM[k] = c_aer_out / mfr.c_aer
+
+    return MFR_SIM
