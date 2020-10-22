c06d49dbbb0cd0bbf6ec393bea712528b3621cb3
J Kodros <jkkodros.research@gmail.com>
2020-10-22 11:38:54 +0300

    Cleaned up fluxes. Will need to look at this further

--- src/fluxes.py
+++ src/fluxes.py
@@ -4,72 +4,53 @@ Created on Wed Sep 11 12:44:43 2019
 
 Fluxes size dist
 
-@author: J Kodros
+@author:
 """
-
-from pylab import *
-
-def calc_fluxes(t, y, dt, l_heat, t_res_heat, T_f, T_i, n_tot0, pstar\
-                , dHvap, T_ref, MW, sigma, rho, Dn, mu, press, alpha, R):
+import numpy as np
 
 
+def calc_fluxes(t, y, dt, length, t_res, T_f, T_i, n_tot0, pstar,
+                dh_vap, T_ref, MW, sigma, rho, Dn, mu, press, alpha, R):
+    '''
+    This is the main function for the ode to calculate particle flux
+    through the TD. It is largely unchanged from the original model.
+    '''
+    # Particle and gas concentration
     Pc = y[0]
     Gc = y[1]
-
-    #xcoord = l_heat*t/t_res_heat
-    T_TDc = T_f
-
-    #xcoord0 = l_heat*(t-dt)/t_res_heat
-    #T_TD0 = T_f
-
     # Total aerosol number concentation
-    n_tot = n_tot0 * T_i/T_TDc
-
+    n_tot = n_tot0 * T_i/T_f
     # Check for negatives and/or nans
-    Pc = ma.masked_less(Pc, 0).filled(0.0)
-    Pc = ma.masked_invalid(Pc).filled(0.0)
-    Gc = ma.masked_less(Gc, 0).filled(0.0)
-    Gc = ma.masked_invalid(Gc).filled(0.0)
-
+    Pc = np.ma.masked_less(Pc, 0).filled(0.0)
+    Pc = np.ma.masked_invalid(Pc).filled(0.0)
+    Gc = np.ma.masked_less(Gc, 0).filled(0.0)
+    Gc = np.ma.masked_invalid(Gc).filled(0.0)
     # Equilibrium pressures at the TD temperature
-    psat = pstar * exp(dHvap * ((1.0/T_ref) - (1.0/T_TDc))/R)
-    csat = MW * psat / (R * T_TDc)
-
-    # Total particle mass
-    mp = Pc
-
+    psat = pstar * np.exp(dh_vap * ((1.0/T_ref) - (1.0/T_f))/R)
     # Diffusion coefficients of the species
-    D = Dn * (T_TDc/T_ref)**mu
+    D = Dn * (T_f/T_ref)**mu
 
-    #### Calculating the composition
-    if mp > 1.0E-22:
-        #n_apu = 1.0/MW
-
+    # Calculating the composition
+    if Pc > 1.0E-22:
         # Particle volume and size
-        vp = mp/rho
-        rp = (3.0 * vp / (4.0*pi))**(1.0/3.0)
-
+        vp = Pc/rho
+        rp = (3.0 * vp / (4.0*np.pi))**(1.0/3.0)
         # Kelvin effect
-        Ke = exp(2.0 * MW * sigma / (R*T_TDc*rho*rp))
-
+        Ke = np.exp(2.0 * MW * sigma / (R*T_f*rho*rp))
         # Equilibrium pressures
         peq = psat * Ke
-
-        ### Transitional correction
+        # Transitional correction
         # Mean velocity of the gas molecules
-        c_avg = (8.0 * R * T_TDc / (MW * pi)) ** (0.5)
-
+        c_avg = (8.0 * R * T_f / (MW * np.pi)) ** (0.5)
         # mean free path of the gas molecules
         lambda_mfp = 3.0 * D / c_avg
-
         # Knudsen number
         Kn = lambda_mfp / rp
-
         # Fucks and Sutugin transition regime correction
-        beta = (1.0 + Kn) / (1.0 + (4.0/(3.0*alpha) + 0.377)*Kn + 4*Kn**2/(3.0*alpha))
-
+        beta = ((1.0 + Kn) /
+                (1.0 + (4.0/(3.0*alpha) + 0.377)*Kn + 4*Kn**2/(3.0*alpha)))
     else:
-        mp = 0.0
+        Pc = 0.0
         vp = 0.0
         rp = 0.0
         peq = 0.0
@@ -77,14 +58,13 @@ def calc_fluxes(t, y, dt, l_heat, t_res_heat, T_f, T_i, n_tot0, pstar\
 
     # pressure at the particle surface
     pv_a = peq
-
     # partial pressure far away from the particles
-    pv_i = Gc * R * T_TDc / MW
-
-    if mp > 0.0 and (1.0 - pv_a/press)/(1.0 - pv_i/press) > 0.0:
-        flx = 4.0 * pi * rp * press *D * beta * MW * log((1.0 - pv_a/press)/(1.0 - pv_i/press))/(R*T_TDc)
-    elif mp > 0.0 and (1.0 - pv_a/press)/(1.0 - pv_i/press) <= 0.0:
-        flx = 4.0 * pi * rp * D * beta * MW * (pv_i-pv_a)/(R*T_TDc)
+    pv_i = Gc * R * T_f / MW
+    if Pc > 0.0 and (1.0 - pv_a/press)/(1.0 - pv_i/press) > 0.0:
+        flx = (4.0 * np.pi * rp * press * D * beta * MW *
+               np.log((1.0 - pv_a/press)/(1.0 - pv_i/press))/(R*T_f))
+    elif Pc > 0.0 and (1.0 - pv_a/press)/(1.0 - pv_i/press) <= 0.0:
+        flx = 4.0 * np.pi * rp * D * beta * MW * (pv_i-pv_a)/(R*T_f)
     else:
         flx = 0.0
 
@@ -92,8 +72,6 @@ def calc_fluxes(t, y, dt, l_heat, t_res_heat, T_f, T_i, n_tot0, pstar\
     flx2 = -1 * n_tot * flx
 
     # Check for bad values
-    output = array([flx, flx2])
+    output = np.array([flx, flx2])
 
     return output
-
-
